9d3e97833b3def9777685ee08da6acad0e9ed479
Stefan Foerth de Araujo <stefanfoerth@hotmail.com>
2021-09-17 23:01:24 -0300

    Teste 2

--- CentralAlarmes/FrmPanelViewer.Designer.cs
+++ CentralAlarmes/FrmPanelViewer.Designer.cs
@@ -202,7 +202,7 @@ namespace PanelManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1062, 471);
+            this.ClientSize = new System.Drawing.Size(974, 471);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.GrpEvents);
             this.Controls.Add(this.GrpCentrals);
